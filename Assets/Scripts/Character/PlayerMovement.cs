f506f70abf52f7d40be78b190e91e50b905db1d0
whattaclay <dimakorzhnev@mail.ru>
2023-07-05 16:40:09 +0300

    добавил анимацию падения и немного логики ползания

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Character.CharactersMagicBall;
 using UnityEngine;
@@ -10,14 +11,19 @@ namespace Character
         [SerializeField] private DistanceAttack distanceAttack;
         [SerializeField] private Animator animator;
         [SerializeField] private float runSpeed = 40f;
+        private Rigidbody2D _rigidBody;
         private const float ShootAnimationDelay = 0.5f;
+        private const float FallingConst = -0.8f;
         private float _horizontalMove = 0f;
         private bool _isCrouch = false;
+        private bool _isCharacterStillCrouching = false;
         private bool _isJump = false;
         private bool _isShooting = false;
-                /// <summary>
-                /// TODO анимацию и логику падения
-                /// </summary>
+
+        private void Start()
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+        }
         void Update()
         {
             _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //задем значение горизонтального перемещения
@@ -37,12 +43,17 @@ namespace Character
             {
                 _isCrouch = false;
             }
-            if (Input.GetButtonDown("Fire1")&& !distanceAttack.IsAttackCd() && !_isCrouch && !_isJump && !_isShooting)// если персонаж нажал клавишу выстрела,
+            if (Input.GetButtonDown("Fire1")&& !distanceAttack.IsAttackCd() && !_isCrouch && !_isJump && !_isShooting && !_isCharacterStillCrouching)// если персонаж нажал клавишу выстрела,
             {                                               //не идет кд после выстрела, персонаж не ползет,не прыгает и не стреляет, то выполняется выстрел
                 _isShooting = true;
                 animator.SetBool("IsShooting", _isShooting);
                 distanceAttack.Shoot();
                 StartCoroutine(AnimationDelay());
+            }
+            animator.SetBool("IsFalling", _rigidBody.velocity.y < FallingConst); //задаем в аниматор булевую переменную падения, если скорость по вертикали отрицательная
+            if (_rigidBody.velocity.y < FallingConst)
+            {
+                animator.SetBool("IsJumping", false); //выключаем анимацию прыжка, если персонаж падает
             }
         }
         private IEnumerator AnimationDelay() //корутина для анимации выстрела, после делея выключает анимацию и устанавливает булевый параметр, что персонаж не стреляет
@@ -51,14 +62,14 @@ namespace Character
             _isShooting = false;
             animator.SetBool("IsShooting", _isShooting);
         }
-        public void OnLanding() //метод прокидывается в ивент, срабатывающий когда персонаж контактирует с коллизией земли. выключаем анимацию прыжка
+        public void OnLanding() //метод прокидывается в ивент, срабатывающий когда персонаж контактирует с коллизией земли
         {
-            animator.SetBool("IsJumping", false);
             _isJump = false;
         }
         public void IsCrouching(bool isCrouching)
         {
-            animator.SetBool("IsCrouch", isCrouching);
+            animator.SetBool("IsCrouch", isCrouching); //влючаю анимацию ползания, в зависимости от ивента  ползания
+            _isCharacterStillCrouching = isCrouching;// делаем вторую проверку на ползание, чтобы не уметь стрелять при ползке
         }
         private void FixedUpdate()
         {
